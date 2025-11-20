a485a33c9e2421fbc9fece6aa2df0fb3ab9eae16
Matthew Printz <matt@jataware.com>
2025-11-20 10:48:54 -0700

    Allow auto-discovery/importing from both entrypoints and json files

--- beaker_kernel/lib/autodiscovery.py
+++ beaker_kernel/lib/autodiscovery.py
@@ -1,10 +1,13 @@
+import importlib
 import json
 import logging
 import os
 import sys
 import typing
-from collections.abc import Mapping
+import warnings
+from collections.abc import Mapping, ItemsView
 from importlib.metadata import entry_points, EntryPoints, EntryPoint
+from traceback import format_exc
 
 
 logger = logging.getLogger(__name__)
@@ -89,26 +92,86 @@ class AutodiscoveryItems(Mapping[str, type]):
     raw: EntryPoints
     mapping: dict[str, EntryPoint]
 
+    # Temporary transitional storage for use while migrating from json files to entrypoints
+    raw_jsons: dict[str, type|dict[str, str]]
+
+    class AutodiscoveryItemsView(ItemsView):
+        """
+        A view class that overrides the default ItemsView to handle exceptions during iteration.
+        Prevents the entire application from failing if an extension cannot be loaded.
+        """
+        def __init__(self, mapping: "AutodiscoveryItems"):
+            super().__init__(mapping)
+
+        def __iter__(self):
+            for key in self._mapping:
+                try:
+                    yield (key, self._mapping[key])
+                except Exception as err:
+                    output = [
+                        f"Unable to load autodiscovery item '{key}'. Error: {err}",
+                         "  Exception traceback when loading item:",
+                        f"  ================ Traceback Start ================",
+                    ]
+                    indented_tb = [f"    {line}" for line in format_exc().splitlines()]
+                    output.extend(indented_tb)
+                    output.append(
+                        f"  ================ Traceback Done =================",
+                    )
+                    logger.warning("\n".join(output))
+                    continue
+
     def __init__(self, entrypoints_instance: EntryPoints):
         self.raw = entrypoints_instance
         self.mapping = {
             item.name: item for item in self.raw
         }
+        self.raw_jsons = {}
 
     def __getitem__(self, key):
-        try:
-            item: EntryPoint = self.mapping.get(key, None)
-            if item:
-                item = item.load()
-        except ImportError:
-            raise
+        # Loading from etrypoints is the new preferred method.
+        # Load class from entrypoint
+        item: EntryPoint = self.mapping.get(key, None)
+        if item:
+            item = item.load()
+            return item
 
-        return item
+        # Fallback to loading from old json file
+        item = self.mapping.get(key, self.raw_jsons.get(key))
+        if isinstance(item, (str, bytes, os.PathLike)) and os.path(path := os.fspath(item)) and path.endswith('.json'):
+            with open(path) as jsonfile:
+                item = json.load(jsonfile)
+                item["mapping_file"] = path
+        match item:
+            case type():
+                return item
+            case {"slug": slug, "package": package, "class_name": class_name, **kw}:
+                mapping_file = kw.get("mapping_file", None)
+                module = importlib.import_module(package)
+                assert slug == key, f"Autoimported item's slug ('{slug}') does not match key ('{key}')"
+                discovered_class = getattr(module, class_name)
+                if mapping_file:
+                    setattr(discovered_class, '_autodiscovery', {
+                        "mapping_file": mapping_file,
+                        **item
+                    })
+                self.mapping[key] = discovered_class
+                return discovered_class
+            case _:
+                raise ValueError(f"Unable to handle autodiscovery item '{item}' (type '{item.__class__}')")
+
+    def add_json_mapping(self, key: str, value: type|dict[str, str]):
+        self.raw_jsons[key] = value
+
     def __iter__(self):
         yield from self.mapping.keys()
+        yield from self.raw_jsons.__iter__()
+
+    def items(self):
+        return self.AutodiscoveryItemsView(self)
 
     def __len__(self):
-        return len(self.raw)
+        return len(self.raw) + len(self.raw_jsons)
 
 
 def autodiscover(mapping_type: ResourceType) -> typing.Dict[str, type]:
@@ -117,4 +180,17 @@ def autodiscover(mapping_type: ResourceType) -> typing.Dict[str, type]:
     """
     group = f"beaker.{mapping_type}"
     eps = entry_points(group=group)
-    return AutodiscoveryItems(eps)
+    items: AutodiscoveryItems = AutodiscoveryItems(eps)
+
+    # Add legacy json mappings
+    for mapping_file, data in find_mappings(mapping_type):
+        slug = data["slug"]
+        items.add_json_mapping(slug, {"mapping_file": mapping_file, **data})
+        warnings.warn(
+            (
+                f"Beaker is loading {mapping_type} from legacy JSON mapping file {mapping_file}.\n"
+                f"    This package should be rebuilt using entrypoints for better performance and reliability."
+            ),
+            DeprecationWarning
+        )
+    return items
