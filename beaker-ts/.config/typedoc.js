aedba4db8143dedc4c4cfb2ad12a35c21695b537
Matthew Printz <matt@jataware.com>
2026-05-29 16:06:55 -0600

    Reenable typedoc output

--- beaker-ts/.config/typedoc.js
+++ beaker-ts/.config/typedoc.js
@@ -1,5 +1,5 @@
 /** @type {import('typedoc').TypeDocOptions} */
-module.exports = {
+export default {
     entryPoints: ["../src/index.ts", "../src/session.ts", "../src/notebook.ts", "../src/util.ts", "../src/history.ts"],
     //entryPoints: ["../src/index.ts", "../src/session.ts", "../src/notebook.ts", ],
     out: "../../docs/beaker-ts",
